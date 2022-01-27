b3d658741e446db40d63aad57ac8089479482e16
Henry Ren <henryren.hry@gmail.com>
2022-01-27 09:38:42 -0500

    Format

--- src/archfetch.c
+++ src/archfetch.c
@@ -1,14 +1,14 @@
 #define _POSIX_C_SOURCE 200809L
 
-#include "color.h"
-
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/sysinfo.h>
-#include <sys/utsname.h>
 #include <string.h>
-#include <limits.h>
+#include <sys/sysinfo.h>
 #include <sys/types.h>
+#include <sys/utsname.h>
+
+#include "color.h"
 
 char *username, *osname, *shellname, *de, *kver;
 long utH, utM;
@@ -24,15 +24,15 @@ void *uptime() {
 	struct sysinfo sInfo;
 	sysinfo(&sInfo);
 	int t = sInfo.uptime;
-	utH = (t % 86400)/3600;
-	utM =  (t % 3600)/60;
+	utH = (t % 86400) / 3600;
+	utM = (t % 3600) / 60;
 	return NULL;
 }
 
 void *user() {
 	char hostname[HOST_NAME_MAX + 1];
 	gethostname(hostname, HOST_NAME_MAX + 1);
-	username = strcat(getenv("USER"),"@");
+	username = strcat(getenv("USER"), "@");
 	username = strcat(username, hostname);
 	return NULL;
 }
@@ -68,22 +68,26 @@ int main() {
 	pthread_create(&threads[5], NULL, desktop, NULL);
 
 	pthread_join(threads[0], NULL);
-	/* os function must be run to get info.col1 */
+
 	pthread_join(threads[1], NULL);
-	printf("%s\n","");
-	printf("%s    %s%s%s\n", BLUE "      /\\      ", "   USER ", WHITE, username);
+	printf("%s\n", "");
+	printf("%s    %s%s%s\n", BLUE "      /\\      ", "   USER ", WHITE,
+		   username);
 	printf("%s    %s%s%s\n", BLUE "     /  \\     ", "     OS ", WHITE, osname);
 	pthread_join(threads[2], NULL);
 	printf("%s    %s%s%s\n", BLUE "    /\\   \\   ", "  KERNEL ", WHITE, kver);
 	pthread_join(threads[3], NULL);
-	printf("%s    %s%s%ldh %ldm\n", BLUE "   /      \\   ", " UPTIME ", WHITE, utH,utM);
+	printf("%s    %s%s%ldh %ldm\n", BLUE "   /      \\   ", " UPTIME ", WHITE,
+		   utH, utM);
 	pthread_join(threads[4], NULL);
-	printf("%s    %s%s%s\n", BLUE "  /   ,,   \\  ", "  SHELL ", WHITE, shellname);
+	printf("%s    %s%s%s\n", BLUE "  /   ,,   \\  ", "  SHELL ", WHITE,
+		   shellname);
 	printf("%s    %s%s%s\n", BLUE " /   |  |  -\\ ", "     DE ", WHITE, de);
-	printf("%s    %s%s%s\n", BLUE "/_-''    ''-_\\", "   PKGS ", WHITE, "Not Supported");
+	printf("%s    %s%s%s\n", BLUE "/_-''    ''-_\\", "   PKGS ", WHITE,
+		   "Not Supported");
 	printf("%s\n", "");
 
 	pthread_join(threads[5], NULL);
-	//int r=system("cat /etc/os-release");
+	// int r=system("cat /etc/os-release");
 	return 0;
 }
